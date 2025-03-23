80d3b5db3f3bdf4f4096818ceaf5f63d044b023d
yankbg <yankbg2@gmail.com>
2025-03-23 22:56:08 +0300

    Commit#1#5
    in the NV, online we add private keyword on an instance variable, online 6-8 we created a constructor of this class

--- Manage-exchange/src/main/java/Manage/CurrencyConverter.java
+++ Manage-exchange/src/main/java/Manage/CurrencyConverter.java
@@ -1,7 +1,12 @@
 package Manage;
 import exchange.exchangerate;
 public class CurrencyConverter {
-    exchangerate Exchangerate;
+   private exchangerate Exchangerate;
+
+    public CurrencyConverter(exchangerate exchangerate) {
+        this.Exchangerate = exchangerate;
+    }
+
     public double convert(double amount, String fromCurrency, String toCurrency) {
         double rate = Exchangerate.getExchangeRate(fromCurrency, toCurrency);
         return amount * rate;
