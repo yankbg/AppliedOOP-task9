cf99d45ea80e2818bf275db0ff79117355ef9044
yankbg <yankbg2@gmail.com>
2025-03-23 21:53:45 +0300

    Commit#1#4
    in the NV, online 4 we declared a instance variable, online 5-8 we created a method called convert

--- Manage-exchange/src/main/java/Manage/CurrencyConverter.java
+++ Manage-exchange/src/main/java/Manage/CurrencyConverter.java
@@ -1,5 +1,9 @@
 package Manage;
 import exchange.exchangerate;
 public class CurrencyConverter {
-   
+    exchangerate Exchangerate;
+    public double convert(double amount, String fromCurrency, String toCurrency) {
+        double rate = Exchangerate.getExchangeRate(fromCurrency, toCurrency);
+        return amount * rate;
+    }
 }
