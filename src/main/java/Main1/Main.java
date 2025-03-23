b3086df2c4e65b7bf221155327c2e0f09fd2f68d
yankbg <yankbg2@gmail.com>
2025-03-23 22:59:38 +0300

    Commit#2#5
    in the NV, online 8 we created an instance of a class,  we created a variable to store a return value from the convert method.

--- src/main/java/Main1/Main.java
+++ src/main/java/Main1/Main.java
@@ -1,7 +1,12 @@
 package Main1;
+import exchange.exchangerate;
+import Manage.CurrencyConverter;
 
 public class Main {
     public static void main(String[] arg){
         
+        CurrencyConverter currency = new CurrencyConverter(new exchangerate());
+        double balance =currency.convert(200,"USD","CDF");
+        System.out.println("After conversion your balance is "+balance+" CDF");
     }
 }
